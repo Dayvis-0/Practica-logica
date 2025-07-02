9d9f68323a90f78acbc345017f6d32cacba99c49
dayvis <amdayvis39@gmail.com>
2025-07-01 20:57:19 -0500

    01/07/2025

--- Leetcode/Java/LeetcodeJava/src/TwoSum_1.java
+++ Leetcode/Java/LeetcodeJava/src/TwoSum_1.java
@@ -6,7 +6,12 @@ Input: nums = [2,7,11,15] target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0,1]*/
 public class TwoSum_1 {
-    public int[] twosum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         
+        return;
+     }
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("String");
     }
 }
