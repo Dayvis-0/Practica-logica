51f246c714b0e7faf718c35ec5607dd0a035d738
dayvis <amdayvis39@gmail.com>
2025-08-08 17:53:09 -0500

    08/08/2025

---
+++ Leetcode/Java/LeetcodeJava/src/MedianOfTwoSortedArray_4.java
@@ -0,0 +1,15 @@
+/*Given two sorted arrays nums1 and nums2 of size m and n repsectively, return the median of the two sorted arrays.
+The overwall run time complexity should be O(log (m+n))
+
+Example 1:
+Input: num1 = [1,3], num2 = [2]
+Explanation: merged array = [1,2,3] and median is 2
+
+Example 2:
+Input: num1 = [1,2], num2 = [3,4]
+Explanation: merged array = [1,2,3,4] and median is (2+3)/2 = 2.5*/
+public class MedianOfTwoSortedArray_4 {
+    public static double findMedianSortedArrays (int[] nums1, int[] nums2) {
+        return 0.2;
+    }
+}
