6e9dd5410d23def72132424b715d720a40b094f1
Dayvis <amdayvis39@gmail.com>
2025-09-18 18:02:19 -0500

    18/09/2025

---
+++ Leetcode/JavaScript/ChatGPT/1. Variables.js
@@ -0,0 +1,11 @@
+/*You have 3 glasses represented by variables:
+* Challenge: Swap the contents so that:
+- glass 1 ends up empty
+- glass 2 ends up with water
+- glass 3 ends up with juice
+But you can only use one extra temporary variable to make the swap*/
+let glass1 = "water";
+let glass2 = "juice";
+let glass3 = "empty";
+let temp = glass1;
+
